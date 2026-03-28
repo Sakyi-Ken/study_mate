77ef6c54972cbfd5c920c512d7f806e7ece09cff
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2026-03-28 15:43:17 +0000

    Add default value for rag_api_base_url in Settings configuration

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     ghana_nlp_api_key_primary: str = ""
     ghana_nlp_api_key_secondary: str = ""
     ghana_nlp_subscription_key: str = ""
+    rag_api_base_url: str = "https://pinecone-pipeline.onrender.com"
     
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
